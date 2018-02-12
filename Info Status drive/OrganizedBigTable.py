a41b8e0554a200890c516ee0433357d5b3306eda
Bryan Li <bryanli.ca@gmail.com>
2018-02-12 01:53:14 -0500

    update notebooks up to POS features, WIP syntactic features

---
+++ Info Status drive/OrganizedBigTable.py
@@ -0,0 +1,176 @@
+'''
+This file converts data from BigTable to a text file that can be read in by
+StanfordNLP, and allows for writing data from StanfordNLP back into the bigtable.
+'''
+from enum import Enum
+import pandas as pd
+import string
+
+CURRENT_SPEAKER = 'speaker1'
+FILLERS = ['um','uh','uh-huh','hm','ah','mm','mmhm']
+HYPHEN = '-'
+SESSION_NUMBER = 'session_number'
+SPEAKER_A = 'A'
+START_TIME = 'word_start_time'
+TABLE_NAME = 'big-table.csv'
+TURN_INDEX = 'word_number_in_turn'
+TURN_LENGTH = 'total_number_of_words_in_turn'
+WORD = 'word'
+
+'''
+OrderType represents different options for organizing turns from the BigTables.
+'''
+class OrderType(Enum):
+    SPEAKER_A = 1
+    SPEAKER_B = 2
+    INTERPOLATED = 3
+
+'''
+OrganizedBigTable correlates information from StanfordNLP to the Bigtable
+by arranging the bigtable in ways that make it conducive for processing by StanfordNLP,
+and offering methods to write information gotten from Stanford to the bigtable.
+'''
+class OrganizedBigTable(object):
+    '''
+    Constructor for OrganizedBigTable.
+    :param: session_number the session number for which to organize the BigTable.
+    '''
+    def __init__(self, session_number):
+        self.session_number = session_number
+        self.df = pd.read_table(TABLE_NAME, sep = ',')
+        self.spA_turns, self.spB_turns, self.interpolated_turns = _orderBigtableRows(self.df, session_number)
+
+    '''
+    Writes the text of the bigtable a file for processing by Stanford.
+    :param order_type: how to order the text of the bigtable into turns
+    '''
+    def exportToFile(self, order_type):
+        text = _turnsToText(self.spA_turns) if order_type == OrderType.SPEAKER_A \
+               else _turnsToText(self.spB_turns) if order_type == OrderType.SPEAKER_B \
+               else _turnsToText(self.interpolated_turns)
+        target = open(str(self.session_number).zfill(2) + '_' + order_type.name + '.txt', 'w')
+        target.write(text)
+        target.close()
+
+    '''
+    Takes information about this table ordered according to the order type, and
+    adds it as columns to the bigtable.
+    :param order_type: the OrderType of the text file Stanford processed
+    :param column_data: The data with which to populate the new column. Each
+    element of the list should be a 2-element tuple, with the word at the 0th
+    index, and the data for the new column in the following index.
+    :param column_names: the names to give the new column.
+    '''
+    def addColumnToDataFrame(self, order_type, column_data, column_name):
+
+        turns = self.spA_turns if order_type == OrderType.SPEAKER_A \
+                else self.spB_turns if order_type == OrderType.SPEAKER_B \
+                else self.interpolated_turns
+
+        bigtable_rows = [row for turn in turns for row in turn[1]]
+
+        stanford_index_cur = 0
+
+        # add to the table column
+        for row in bigtable_rows:
+            bigtable_word = row[WORD]
+            bigtable_word_char_count = len(bigtable_word)
+            stanford_char_count = 0
+            stanford_index_start = stanford_index_cur
+            # print column_data[stanford_index_cur], bigtable_word
+            # find the range of indices in Stanford that correspond to one word in the bigtable
+            while (stanford_char_count < bigtable_word_char_count and
+                   stanford_index_cur < len(column_data) - 1):
+                stanford_char_count += len(column_data[stanford_index_cur][0])
+                stanford_index_cur += 1
+
+            # Concatenate data from Stanford so can be placed in one row when necessary
+            column_datum = ''
+            for i in range(stanford_index_start, stanford_index_cur):
+                out = column_data[i][1].strip().translate(None, string.punctuation)
+                column_datum += '_' + out if (out != '' and i != stanford_index_start) else out
+
+            self.df.loc[self.df.index[int(row.name)], column_name] = column_datum
+
+    def saveToCSV(self):
+        self.df.to_csv('big-table.csv', sep=',', index=False)
+
+'''
+Helper method to organize turns into a text file with sentences.
+:param turns: a list of turns, organized by time
+:return: a string of text with sentence divisions.
+'''
+def _turnsToText(turns):
+    text = ''
+    for turn in turns:
+        for row in turn[1]:
+            text += ' ' + row[WORD]
+        text += '.'
+    return text
+
+'''
+Represents the rows of the bigtables ordered in the arrangement they are ordered
+for Stanford.
+:return: 3 lists, one with speaker A's turns organized by start time,
+second with Speaker B's sorted by start time, third with the interpolated turns.
+'''
+def _orderBigtableRows(df, session_number):
+    spA_rows, spB_rows = _getSortedSpeakerRows(session_number, df)
+    spA_turns = sorted(_findTurns(spA_rows), key=lambda x: x[0])
+    spB_turns = sorted(_findTurns(spB_rows), key=lambda x: x[0])
+    interpolated_turns = sorted(spA_turns + spB_turns, key=lambda x: x[0])
+    return spA_turns, spB_turns, interpolated_turns
+
+'''
+Arranges ordered rows of a speaker into turns.
+:param speaker_rows: the rows of the speaker
+:return: a list of turns, where each turn is an ordered list of rows
+'''
+def _findTurns(speaker_rows):
+    turns = []
+    turn_list = []
+    for row in speaker_rows:
+        if row[WORD] not in FILLERS and row[WORD][-1] != HYPHEN:
+            turn_list.append(row)
+            if row[TURN_INDEX] == row[TURN_LENGTH]:
+                turns.append([turn_list[0][START_TIME],turn_list])
+                turn_list = []
+    return turns
+
+'''
+Separates words spoken by Speaker A and Speaker B, and sorts them by time.
+:param session: the session to sort
+:return: two lists, one containing the sorted rows of speaker A, the second B
+'''
+def _getSortedSpeakerRows(session, df):
+    spA_rows, spB_rows = [], []
+    j = 0
+    for index, row in df.iterrows():
+        if row[SESSION_NUMBER] == int(session):
+            if row[CURRENT_SPEAKER] == SPEAKER_A:
+                spA_rows.append(row)
+            else: # speaker B
+                spB_rows.append(row)
+        j += 1
+
+    spA_rows = sorted(spA_rows, key=lambda x: x[START_TIME])
+    spB_rows = sorted(spB_rows, key=lambda x: x[START_TIME])
+    return spA_rows, spB_rows
+
+def _getSortedSpeakerRows2(session, df):
+    # fix method to properly sort rows
+    def strip_time(text):
+        idx = text.rfind('.', 0, text.rfind('.'))
+        return text[0:idx]
+    
+    df_temp = df[df[SESSION_NUMBER] == int(session)]
+    dfA = df_temp[df_temp[CURRENT_SPEAKER]=='A'].copy()
+    dfB = df_temp[df_temp[CURRENT_SPEAKER]=='B'].copy()
+    
+    
+    dfA['token_id2']= dfA['token_id'].apply(strip_time)
+    dfB['token_id2']= dfB['token_id'].apply(strip_time)
+        
+    dfA = dfA.sort_values(['token_id',START_TIME],ascending=[True, True])
+    dfB = dfA.sort_values(['token_id',START_TIME],ascending=[True, True])
+    return dfA, dfB
---
+++ Info Status drive/StanfordDocumentInformation.py
@@ -0,0 +1,139 @@
+'''
+StanfordDocumentInformation stores  information from Stanford about a file.
+Right now it stores coreferent data (I will add POS info from savePoS.py and syntax information)
+It takes the coreferences for a particular document's XML output
+(from Stanford CoreNLP) and stores them as Mention objects, which contain
+the start char of the word in the document, its end char, and its coreference ID
+for the chain of coreferences.
+'''
+import xml.etree.ElementTree as ET
+
+# for accessing the XML files
+XML_DIR = 'xml/'
+XML_EXTENSION = '_INTERPOLATED.xml'
+# for accessing the document in the XML
+DOCUMENT = 'document'
+
+# for accessing the coreferent information and tags in the XML
+COREFERENCE = 'coreference'
+MENTION = 'mention'
+START = 'start'
+END = 'end'
+
+# for accessing the sentence information and tags in the XML
+SENTENCES = 'sentences'
+SENTENCE = 'sentence'
+TOKENS = 'tokens'
+TOKEN = 'token'
+WORD = 'word'
+ID = 'id'
+
+# for accessing part of speech information
+POS_SUFFIX = '_INTERPOLATEDwords.txt'
+
+'''
+Represent a token in a document, with its token id in the sentence.
+'''
+class Token(object):
+    def __init__(self, token_id, word):
+        self.id = int(token_id)
+        self.coref_id = ''
+        self.word = word
+
+'''
+Take sentence data from the XML and organizes into a dictionary,
+where keys are sentence IDs, and values are a list of Tokens in that
+sentence (in Token object form)
+:param: sentence_data the information under the <sentences> tag in the XML
+:return: the dictionary described above
+'''
+def _OrganizeSentenceData(sentence_data):
+    sentences_dict = {}
+    for sentence in sentence_data:
+        sentence_ID = int(sentence.get(ID))
+        xml_tokens = sentence.find(TOKENS).findall(TOKEN)
+        sentences_dict[sentence_ID] = [Token(xml_token.get(ID),
+                                  xml_token.find(WORD).text)
+                                  for xml_token in xml_tokens]
+
+    # change to list
+    sentences = [None] * (len(sentences_dict))
+    for key in sentences_dict:
+        sentences[key-1] = sentences_dict[key]
+    return sentences
+
+'''
+From XML data about coreferent information and a dictionary of sentences
+with character information, associate coreference data with character offsets.
+:param: coref_data the coreference information under the first <coreference>
+tag in the XML
+:return: a list of all mentions in the document, each with an ID that is
+equal to the ID of other mentions
+'''
+def _AssociateCoreferentData(coreference_data, sentences, mention_offset):
+    mentions = []
+
+    coref_id = mention_offset # ensures unique identity across sessions
+
+    for coreference in coreference_data:
+        # we go through every mention for the coreferent ID
+        xml_mentions = coreference.findall(MENTION)
+        for xml_mention in xml_mentions:
+            sentence_ID = int(xml_mention.find(SENTENCE).text)
+            start_word, end_word = xml_mention.find(START).text, xml_mention.find(END).text
+            # getting the Token object and storing coreference ID
+            tokens = sentences[sentence_ID - 1]
+            for token in tokens:
+                if token.id >= int(start_word) and token.id < int(end_word):
+                    token.coref_id = str(coref_id)
+            sentences[sentence_ID - 1] = tokens
+        coref_id+=1
+    return sentences, coref_id
+
+'''
+Represent coreferent information about a document.
+'''
+class StanfordDocumentInformation(object):
+
+    '''
+    Constructor for document information.
+    :param: num the number of the document
+    :param: the offset at which to start coreferent IDs (to avoid duplicate IDs)
+    '''
+    def __init__(self, session_number, mention_offset):
+        # document parse
+        TREE = ET.parse(XML_DIR + session_number + XML_EXTENSION).getroot().find(DOCUMENT)
+        # the sentence data has the offset characters, while the coreferent
+        # data has the coreferent mention information we want. So we associate
+        # both and store the resulting mentions.
+        sentence_data = TREE.find(SENTENCES).findall(SENTENCE)
+        sentences = _OrganizeSentenceData(sentence_data)
+        coreference_data = TREE.find(COREFERENCE).findall(COREFERENCE)
+        self.sentences, self.coref_id_end = _AssociateCoreferentData(coreference_data,
+                                                 sentences,
+                                                 mention_offset)
+        self.session_number = session_number
+
+    '''
+    Gets a list of the coreferent words in the document.
+    '''
+    def getCoreferenceList(self):
+        return [(token.word, token.coref_id)
+                for sentence in self.sentences
+                for token in sentence
+                if token.word != '.']
+
+    '''
+    Get parts of speech processed from Stanford as a list of words, parts of speech.
+    NOTE: right now this is still getting data from the Java-generated file rather than
+    the XML parse.
+    '''
+    def getPoSList(self):
+        words_order = []
+        filename = self.session_number + POS_SUFFIX
+        with open(filename) as stanford:
+            for line in stanford:
+                word = tuple(str.split(line))
+                if word[0] != '.':
+                    words_order.append(word)
+        return words_order
