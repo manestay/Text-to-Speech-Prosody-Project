4012c8d657d82819f6ca0b4ae5faa24c11a6533a
Bryan Li <bryanli.ca@gmail.com>
2018-03-02 18:06:19 -0500

    update 20180302 CSV

--- info-status/CombineSessionTables.py
+++ info-status/CombineSessionTables.py
@@ -10,16 +10,17 @@ TODAY = datetime.date.today().strftime("%Y%m%d")
 START_SESSION = 1
 END_SESSION = 13
 
-filename = TABLE_PREFIX + DATE_SESSION + "1" + SUFFIX
+filename = TABLE_PREFIX + DATE_SESSION + "01" + SUFFIX
 df = pd.read_csv(filename)
 
 pos_column = pd.Series([None] * len(df))
 coref_column = pd.Series([None] * len(df))
 
 for session_number in range(START_SESSION, END_SESSION):
-    # session_number = str(session_number).zfill(2)
+    session_number = str(session_number).zfill(2)
 
     filename = TABLE_PREFIX + DATE_SESSION + str(session_number) + SUFFIX
+    print(filename)
     df = pd.read_csv(filename)
 
     pos_column = df['Stanford_PoS'].combine_first(pos_column)
