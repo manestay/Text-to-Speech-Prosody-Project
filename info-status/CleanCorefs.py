aa36d587bbe0a7750ce305e2b934dfb495f0427f
Amaan Khullar <amaankhullar@gmail.com>
2018-04-06 15:50:09 -0400

    Added script that cleans most of the XML Coreferences

---
+++ info-status/CleanCorefs.py
@@ -0,0 +1,27 @@
+import xml.etree.ElementTree as ET
+import os
+
+XML_DIR = "xml-corrections/"
+
+for file in os.listdir(XML_DIR):
+    if file.endswith(".xml"):
+        print("Cleaning " + file)
+        tree = ET.parse(XML_DIR + file)
+        root = tree.getroot()
+
+        corefs = root[0][2]
+
+        for coref in corefs:
+            for mention in coref:
+                start = int(mention.find('start').text)
+                end = int(mention.find('end').text)
+                head = int(mention.find('head').text)
+                text = mention.find('text').text
+
+                if len(text.split()) > 1:
+                    token = text.split()[head - start]
+                    mention.find('start').text = str(head)
+                    mention.find('end').text = str(head + 1)
+                    mention.find('text').text = token
+
+        tree.write(XML_DIR + file)
